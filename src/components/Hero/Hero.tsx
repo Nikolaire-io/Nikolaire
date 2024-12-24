b871b35e657a00743c8b1f72733b7d629da0fc0a
Tarek Siddique Nabil <nabilh420@gmail.com>
2024-12-24 08:53:06 +0600

    fix(word rotate animation issue)

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,29 +1,48 @@
 "use client";
 import { useGSAP } from "@gsap/react";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef } from "react";
 import gsap from "gsap";
 import SplitText from "@gregoire.ciles/split-text";
-import { cn } from "@/lib/utils";
+
+import AnimatedShinyText from "../ui/animated-shiny-text";
 
 const Hero = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const words = ["Web Apps", "Sass", "Big Things"];
+
   useGSAP(() => {
-    const tl = gsap.timeline({ repeat: -1, repeatDelay: 0.2 });
-    const titles = gsap.utils.toArray(".text-wrapper p");
-    titles.forEach((title: any, index: number) => {
-      const splitText = new SplitText(title);
+    // create wrapper element each word
+
+    const elements = words.map((word, index) => {
+      const div = document.createElement("div");
+
+      div.textContent = word;
+      div.className = `absolute top-2.5 lg:top-4 md:top-3.5 w-full lg:text-4xl  md:text-3xl text-4xl font-itim font-medium ${
+        index === 0 && "dark:text-purple-500 text-orange-500"
+      } ${index === 1 && "dark:text-sky-500 text-blue-500"} ${
+        index === 2 && "dark:text-lime-500 text-green-500 "
+      }`;
+      containerRef.current?.appendChild(div);
+      return div;
+    });
+
+    // Create the animation timeline
+    const tl = gsap.timeline({ repeat: -1, repeatDelay: 0.3 });
+
+    elements.forEach((el, index) => {
+      const splitText = new SplitText(el);
       tl.from(
         splitText.chars,
         {
           opacity: 0,
           y: 50,
-
           rotateX: -90,
           stagger: {
             each: 0.05,
             from: "start",
             ease: "power2.inOut",
           },
-          duration: 1,
+          duration: 0.5,
           // repeat: -1,
         },
         "<"
@@ -32,51 +51,53 @@ const Hero = () => {
         {
           opacity: 0,
           y: -50,
-
           rotateX: 90,
           stagger: {
             each: 0.05,
             from: "end",
             ease: "expo.inOut",
           },
-          delay: 1,
+
+          delay: 1.2,
           // repeat: -1,
         },
         "<1"
       );
     });
+
+    // Cleanup
+    return () => {
+      tl.kill();
+      elements.forEach((el) => el.remove());
+    };
   }, []);
+
   return (
-    // className="container mx-auto flex justify-center items-center "
     <section className="mt-16">
       <div className="space-y-5">
-        <p className="font-exo text-muted-foreground text-sm  pl-5">
-          A Top Notch Web Agency{" "}
-        </p>
-        <div className="flex flex-col items-start gap-5">
-          <span className="text-7xl font-semibold font-josefin-sans">
+        <div className="group rounded-full w-56 md:mx-0 mx-auto border-border bg-neutral-100 text-base text-white transition-all ease-in hover:cursor-pointer hover:bg-neutral-200  dark:bg-neutral-900 dark:hover:bg-neutral-800">
+          <AnimatedShinyText className="inline-flex items-center  w-full justify-center px-4 py-1 transition ease-out hover:text-neutral-600 hover:duration-300 hover:dark:text-neutral-400 font-itim">
+            <span className="text-sm lg:text-base">A Top Notch Web Agency</span>
+          </AnimatedShinyText>
+        </div>
+        <div className="flex flex-col place-items-center md:place-items-start gap-2.5  md:gap-5 ">
+          <span className=" lg:text-7xl text-4xl  font-semibold font-josefin-sans  ">
             Build Your
           </span>
-          <div className="flex items-center gap-4">
-            <span className="text-7xl font-semibold font-josefin-sans">
+
+          <div className="flex flex-col md:flex-row gap-1 lg:gap-3 items-center">
+            <span className="lg:text-7xl text-4xl font-semibold font-josefin-sans">
               Dream
             </span>
-            <div className="text-wrapper ">
-              <p className="text-3xl font-amaranth mt-5 leading-[0px]  ">
-                Web Apps
-              </p>
-              <p className="text-3xl font-exo -mt-2 font-semibold leading-[0px]">
-                Sass
-              </p>
-              <p className="text-3xl font-quicksand font-semibold -mt-2 leading-[0px]">
-                Big Things
-              </p>
-            </div>
+            <div
+              ref={containerRef}
+              className="relative md:w-44 w-80 md:text-start text-center h-16   overflow-hidden  "
+              // style={{ lineHeight: "60px", verticalAlign: "bottom" }}
+            />
           </div>
-        </div>
-        <div>
-          <span className="text-4xl font-semibold font-josefin-sans">
-            With Us
+
+          <span className="lg:text-4xl font-semibold  font-josefin-sans">
+            With Us 🫂
           </span>
         </div>
       </div>
