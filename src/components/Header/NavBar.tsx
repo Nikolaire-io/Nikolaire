34557c1862f1935c6fa3d6e33d8106d38b288ef5
Tarek Siddique Nabil <nabilh420@gmail.com>
2024-12-25 17:54:08 +0600

    feat(navbar): Add sidebar sheet for small device

--- src/components/Header/NavBar.tsx
+++ src/components/Header/NavBar.tsx
@@ -10,9 +10,70 @@ import {
 import { cn } from "@/lib/utils";
 import { ModeToggle } from "@/components/Mode-Toggle";
 import { Button } from "../ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "../ui/sheet";
+import Link from "next/link";
 
 const NavBar = () => {
-  const [active, setActive] = useState<boolean>(false);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const navlinks = [
+    {
+      label: "Services",
+      links: [
+        { label: "Web Development", href: "/web-dev" },
+        { label: "Interface Design", href: "/interface-design" },
+        { label: "Search Engine Optimization", href: "/seo" },
+        { label: "Branding", href: "/branding" },
+      ],
+    },
+    {
+      label: "Products",
+      links: [
+        {
+          title: "Algochurn",
+          href: "https://algochurn.com",
+          src: "https://assets.aceternity.com/demos/algochurn.webp",
+          description: "Prepare for tech interviews like never before.",
+        },
+        {
+          title: "Tailwind Master Kit",
+          href: "https://tailwindmasterkit.com",
+          src: "https://assets.aceternity.com/demos/tailwindmasterkit.webp",
+          description:
+            "Production ready Tailwind css components for your next project",
+        },
+        {
+          title: "Moonbeam",
+          href: "https://gomoonbeam.com",
+          src: "https://assets.aceternity.com/demos/Screenshot+2024-02-21+at+11.51.31%E2%80%AFPM.png",
+          description:
+            "Never write from scratch again. Go from idea to blog in minutes.",
+        },
+        {
+          title: "Rogue",
+          href: "https://userogue.com",
+          src: "https://assets.aceternity.com/demos/Screenshot+2024-02-21+at+11.47.07%E2%80%AFPM.png",
+          description:
+            "Respond to government RFPs, RFIs and RFQs 10x faster using AI",
+        },
+      ],
+    },
+    {
+      label: "Pricing",
+      links: [
+        { label: "Hobby", href: "/hobby" },
+        { label: "Individual", href: "/individual" },
+        { label: "Team", href: "/team" },
+        { label: "Enterprise", href: "/enterprise" },
+      ],
+    },
+  ];
 
   return (
     <nav className="fixed inset-x-0 max-w-screen-2xl mx-auto z-50 flex justify-between items-center py-4 px-3 backdrop-blur-xl bg-transparent  border-b border-border rounded-2xl  lg:px-8 h-[76px]">
@@ -20,31 +81,43 @@ const NavBar = () => {
       <NavBarMenu className="bg-transparent " />
       <div className="flex items-center space-x-3">
         <ModeToggle />
-        <Button
-          onClick={() => setActive(!active)}
-          variant={"outline"}
-          size={"icon"}
-          className="md:hidden"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-6  "
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d={`${
-                active
-                  ? "M6 18 18 6M6 6l12 12"
-                  : "M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-              }`}
-            />
-          </svg>
-        </Button>
+
+        <Sheet open={isSidebarOpen} onOpenChange={setIsSidebarOpen}>
+          <SheetTrigger asChild>
+            <Button variant={"outline"} size={"icon"} className="md:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6  "
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d={`${
+                    isSidebarOpen
+                      ? "M6 18 18 6M6 6l12 12"
+                      : "M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                  }`}
+                />
+              </svg>
+            </Button>
+          </SheetTrigger>
+          <SheetContent>
+            <SheetHeader className="mt-4">
+              <SheetTitle className="font-josefin-sans text-xl font-semibold  ">
+                Nikolaire
+              </SheetTitle>
+              <SheetDescription>
+                <p className="text-sm text-gray-500">
+                  We build products that empower people
+                </p>
+              </SheetDescription>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
       </div>
     </nav>
   );
@@ -65,9 +138,13 @@ function NavBarMenu({ className }: { className: string }) {
         <MenuItem setActive={setActive} active={active} item="Services">
           <div className="flex flex-col space-y-4 text-sm">
             <HoveredLink href="/web-dev">Web Development</HoveredLink>
-            <HoveredLink href="/interface-design">Interface Design</HoveredLink>
-            <HoveredLink href="/seo">Search Engine Optimization</HoveredLink>
-            <HoveredLink href="/branding">Branding</HoveredLink>
+            <HoveredLink href="/interface-design">
+              BackEnd Development
+            </HoveredLink>
+            <HoveredLink href="/seo">App Development</HoveredLink>
+            <HoveredLink href="/branding">
+              Infrastructure Development{" "}
+            </HoveredLink>
           </div>
         </MenuItem>
         <MenuItem setActive={setActive} active={active} item="Products">
