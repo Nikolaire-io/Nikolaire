5d513a26c1afb1bf7e5fb53d1255b6dce5febc60
Tarek Siddique Nabil <nabilh420@gmail.com>
2024-12-25 18:29:07 +0600

    update(open-graph): Update Og Image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,38 @@ import NavBar from "@/components/Header/NavBar";
 export const metadata: Metadata = {
   title: "Nikolaire",
   description:
-    "Nikolaire is an award-winning web design agency focused on fostering relationships & work culture to deliver world-class digital products.",
+    "Nikolaire is an award-winning Software Development agency focused on fostering relationships & work culture to deliver world-class digital products.",
+  openGraph: {
+    title: "Nikolaire",
+    description:
+      "Nikolaire is an award-winning web Software Development agency focused on fostering relationships & work culture to deliver world-class digital products.",
+    type: "website",
+    url: "https://nikolaire.com",
+    siteName: "Nikolaire",
+    images: [
+      {
+        url: "./icon.svg",
+        width: 800,
+        height: 600,
+        alt: "Nikolaire",
+      },
+    ],
+  },
+  twitter: {
+    site: "@nikolaire",
+    card: "summary_large_image",
+    title: "Nikolaire",
+    description:
+      "Nikolaire is an award-winning Software Development agency focused on fostering relationships & work culture to deliver world-class digital products.",
+    images: [
+      {
+        url: "./icon.svg",
+        width: 800,
+        height: 600,
+        alt: "Nikolaire",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
